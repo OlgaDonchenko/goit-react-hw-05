a627d418c6cad90f5c516d326dd1ea7d08011e53
OlgaDonchenko <n0931606045@gmail.com>
2024-05-15 18:38:50 +0300

    need to fix structure like body header main

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,22 +5,14 @@ import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
 import MovieCast from "../MovieCast/MovieCast";
 import MovieReviews from "../MovieReviews/MovieReviews";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
-import { Link } from "react-router-dom";
+import Navigation from "../Navigation/Navigation";
+import css from "./App.module.css";
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/movies">Movies</Link>
-            </li>
-          </ul>
-        </nav>
+      <div className={css.container}>
+        <Navigation />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -19,7 +19,7 @@ export default function MovieDetailsPage() {
   const backLinkURL = useRef(location.state ?? "/");
 
   const buildLinkClass = ({ isActive }) => {
-    return clsx(css.btnLink, isActive && css.moreInfoLinkActive);
+    return clsx(css.btnLink, isActive && css.active);
   };
 
   useEffect(() => {
@@ -41,16 +41,13 @@ export default function MovieDetailsPage() {
   const userScore = movie ? (Number(movie.vote_average) * 10).toFixed(0) : null;
 
   return (
-    <section className={css.movieDetails}>
-      <Link to={backLinkURL.current} className={css.btnLink}>
-        Go back
-      </Link>
+    <section>
+      <Link to={backLinkURL.current}>Go back</Link>
       {loading && <p>Loading information...</p>}
       {movie && (
-        <div className={css.movieDetailsSection}>
-          <div className={css.movieDetailsThumb}>
+        <div>
+          <div>
             <img
-              className={css.movieDetailsImg}
               src={
                 movie.poster_path
                   ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
@@ -61,35 +58,29 @@ export default function MovieDetailsPage() {
               height="500"
             />
             <div>
-              <h2 className={css.movieDetailsTitle}>{movie.original_title}</h2>
-              <p className={css.movieDetailsTagline}>{movie.tagline}</p>
-              <p className={css.movieDetailsText}>
-                <span className={css.spanAccent}>Release date:</span>{" "}
-                {movie.release_date}
+              <h2>{movie.original_title}</h2>
+              <p>{movie.tagline}</p>
+              <p>
+                <span>Release date:</span> {movie.release_date}
               </p>
               {userScore !== "0" && userScore !== null && (
-                <div className={css.movieDetailsScore}>
-                  <p className={css.movieDetailsText}>
-                    <span className={css.spanAccent}>User Score:</span>{" "}
-                    {userScore}&#37;
+                <div>
+                  <p>
+                    <span>User Score:</span> {userScore}&#37;
                   </p>
                 </div>
               )}
-              <h3 className={css.movieDetailsTitleFilm}>Overview</h3>
-              <p className={`${css.movieDetailsText} ${css.forLaptop}`}>
-                {movie.overview}
-              </p>
-              <h3 className={css.movieDetailsTitleFilm}>Genres</h3>
-              <ul className={css.movieDetailsGenresList}>
+              <h3>Overview</h3>
+              <p>{movie.overview}</p>
+              <h3>Genres</h3>
+              <ul>
                 {movie.genres.map((genre) => (
-                  <li className={css.movieDetailsText} key={genre.id}>
-                    {genre.name}
-                  </li>
+                  <li key={genre.id}>{genre.name}</li>
                 ))}
               </ul>
             </div>
           </div>
-          <nav className={css.moreInfo}>
+          <nav className={css.castReview}>
             <NavLink className={buildLinkClass} to={`cast`}>
               Cast
             </NavLink>
