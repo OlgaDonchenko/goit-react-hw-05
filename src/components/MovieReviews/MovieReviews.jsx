9b21fbd7c53c811f710dbe1c0dd16d0a9c83cfef
OlgaDonchenko <n0931606045@gmail.com>
2024-05-17 11:50:08 +0300

    fixed review messaage about No reviews available for this movie

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -5,6 +5,8 @@ import { getMovieReviewsById } from "../../movies-api";
 export default function MovieReviews() {
   const { movieId } = useParams();
   const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchReviews = async () => {
@@ -13,23 +15,38 @@ export default function MovieReviews() {
         setReviews(reviewsData);
       } catch (error) {
         console.error("Error fetching reviews:", error.message);
+        setError(error.message);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchReviews();
   }, [movieId]);
 
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
     <div>
       <h2>Reviews</h2>
-      <ul>
-        {reviews.map((review) => (
-          <li key={review.id}>
-            <h3>{review.author}</h3>
-            <p>{review.content}</p>
-          </li>
-        ))}
-      </ul>
+      {reviews.length === 0 ? (
+        <p>No reviews available for this movie.</p>
+      ) : (
+        <ul>
+          {reviews.map((review) => (
+            <li key={review.id}>
+              <h3>{review.author}</h3>
+              <p>{review.content}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
